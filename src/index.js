68c04c780d5ca274e7314b5869b759cb17f3f570
Abhinav Kumar Singh <aks.singh774@gmail.com>
2022-12-25 22:21:26 +0530

    files ready

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+// CSS
+import "./index.css"
 
-function Greeting(){
-  return <h4>this is abhinav and this is my first component</h4>;
+function BookList(){
+  return (
+  <section className='booklist'>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+  </section>
+  );
 }
 
-ReactDom.render(<Greeting/>,document.getElementById('root'));
+const Book = () => {
+  return (
+  <article className='book'>
+    <img src="https://images-eu.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/61kRkfsIMUL._AC_UL600_SR600,400_.jpg" alt=""/>
+    <h1>Do Epic Shit</h1>
+    <h4>Abhinav Singh</h4>
+  </article>
+  );
+}
+
+ReactDom.render(<BookList/>,document.getElementById('root'));
